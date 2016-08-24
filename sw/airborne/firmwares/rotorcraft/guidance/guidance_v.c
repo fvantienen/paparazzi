da5fa5be94338228ace11367179484fb0d93fdc4
Ewoud Smeur <ewoud_smeur@msn.com>
2016-08-24 15:36:49 +0200

    Added vertical (thrust) control
    when the transition percentage > 0, guidance_hybrid_vertical is called.
    This sets the thrust to a fixed value. The curve is already set from
    gudiance_h.
    Also fixed that the hybrid guidance pitch control is not used anymore in
    rc forward.

--- sw/airborne/firmwares/rotorcraft/guidance/guidance_v.c
+++ sw/airborne/firmwares/rotorcraft/guidance/guidance_v.c
@@ -355,7 +355,9 @@ void guidance_v_run(bool in_flight)
         gv_update_ref_from_z_sp(guidance_v_z_sp);
         run_hover_loop(in_flight);
 #if HYBRID_NAVIGATION
-        guidance_hybrid_vertical();
+        if(transition_percentage > 0) {
+          guidance_hybrid_vertical();
+        }
 #endif
       } else if (vertical_mode == VERTICAL_MODE_CLIMB) {
         guidance_v_z_sp = stateGetPositionNed_i()->z;
