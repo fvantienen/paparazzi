a0136306516540b36a8b506987521d4001272186
Freek van Tienen <freek.v.tienen@gmail.com>
2016-09-11 20:56:15 +0200

    Make video better

--- sw/tools/parrot/bebop.py
+++ sw/tools/parrot/bebop.py
@@ -125,7 +125,7 @@ elif args.command == 'upload_file_and_run':
     #check firmware version
     v = parrot_utils.check_version(tn, '').strip()
     print("Checking Bebop firmware version... " + v )
-    if v != '3.2.0':
+    if v != '3.2.0'and v != '3.4.0-RC2':
         print("Error: please upgrade your Bebop firmware to version 3.2.0!")
     else:
         print("Kill running " + f[1] + " and make folder " + args.folder)
