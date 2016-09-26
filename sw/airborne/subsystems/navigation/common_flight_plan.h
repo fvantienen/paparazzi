592531870d0f68571674fae89eab0780de259d37
Freek van Tienen <freek.v.tienen@gmail.com>
2016-09-26 05:25:45 +0200

    Fix flight plan blocks

--- sw/airborne/subsystems/navigation/common_flight_plan.h
+++ sw/airborne/subsystems/navigation/common_flight_plan.h
@@ -44,8 +44,8 @@ void nav_goto_block(uint8_t block_id);
 #define InitStage() nav_init_stage();
 
 #define Block(x) case x: nav_block=x;
-#define NextBlock() { nav_block++; nav_init_block(); }
-#define GotoBlock(b) { nav_block=b; nav_init_block(); }
+#define NextBlock() nav_goto_block(nav_block + 1)
+#define GotoBlock(b) nav_goto_block(b)
 
 #define Stage(s) case s: nav_stage=s;
 #define NextStage() { nav_stage++; InitStage(); }
